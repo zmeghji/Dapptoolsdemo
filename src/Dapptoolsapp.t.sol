95141a157bb20244cef05b0ea02ad3d860939e78
zmeghji <zeeshan_meghji@hotmail.com>
2022-02-07 15:03:20 -0500

    ran dapp init

---
+++ src/Dapptoolsapp.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.6;
+
+import "ds-test/test.sol";
+
+import "./Dapptoolsapp.sol";
+
+contract DapptoolsappTest is DSTest {
+    Dapptoolsapp dapptoolsapp;
+
+    function setUp() public {
+        dapptoolsapp = new Dapptoolsapp();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
